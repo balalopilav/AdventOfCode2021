3474e71d00c3d44c622a0449a06d8082c52c3835
balalopilav <bilalaydin2@gmail.com>
2022-06-05 02:21:39 +0200

    Add day 3

--- src/tag33.java
+++ src/tag33.java
@@ -8,6 +8,20 @@ import java.util.List;
 
 public class tag33 {
 
+	public static int haeufigste_ziffer(List<String> zahlen, int stelle) {
+		int anzahl = 0;
+		
+		for (int i = 0; i < zahlen.size(); i++) {
+			if(zahlen.get(i).substring(stelle).startsWith("1")) {
+				anzahl++;
+			} else {
+				anzahl--;
+			}
+		}
+		
+		return anzahl >= 0? 1:0;
+	}
+	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		File file = new File("./files/tag3.txt");
 		int oxygen = 0, c02 = 0;
@@ -17,67 +31,58 @@ public class tag33 {
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 		    String line;
 		    while ((line = br.readLine()) != null) {
-		    	String[] ziffern = line.split("");
-		    	for (int i = 0; i < ziffern.length; i++) {
-		    		if(ziffern[i].equals("0")) {
-		    			stellen[i] -= 1;
-		    		}else if(ziffern[i].equals("1")) {
-		    			stellen[i] += 1;
-		    		}
-		    	}
 		    	zahlen.add(line);
 		    }
 		}
-		List<String> zahlen1 = zahlen;
+		List<String> zahlenBackup = new LinkedList<>();
+		for (String zahl : zahlen) {
+			zahlenBackup.add(zahl);
+		}
 		
-		for(int i = 0; i < stellen.length-1; i++) {
-			if(stellen[i] >= 0) {
-				for (int j = 0; j < zahlen.size(); j++) {
-					if(zahlen.get(j).substring(i).startsWith("0")) {
-						zahlen.remove(j);
-						j--;
+		for(int i = 0; i < stellen.length; i++) {
+			int zu_behalten = haeufigste_ziffer(zahlenBackup, i);
+			for (int j = 0; j < zahlen.size(); j++) {
+				if(zu_behalten == 1) {
+					if(zahlen.get(j).substring(i).startsWith("0") && zahlenBackup.size() > 1) {
+						zahlenBackup.remove(zahlen.get(j));
 					}
-				}
-			} else {
-				for (int j = 0; j < zahlen.size(); j++) {
-					if(zahlen.get(j).substring(i).startsWith("1")) {
-						zahlen.remove(j);
-						j--;
+				} else {
+					if(zahlen.get(j).substring(i).startsWith("1") && zahlenBackup.size() > 1) {
+						zahlenBackup.remove(zahlen.get(j));
 					}
 				}
 			}
 		}
+		oxygen = Integer.parseInt(zahlenBackup.get(0), 2);
 		
-		oxygen = Integer.parseInt(zahlen.get(0), 2);
+		zahlenBackup = new LinkedList<>(); //reset to clean list
+		for (String zahl : zahlen) {
+			zahlenBackup.add(zahl);
+		} 
 		
-		for(int i = 0; i < stellen.length-1; i++) {
-			if(stellen[i] < 0) {
-				for (int j = 0; j < zahlen1.size(); j++) {
-					if(zahlen1.get(j).substring(i).startsWith("0")) {
-						zahlen1.remove(j);
-						j--;
+		for(int i = 0; i < stellen.length; i++) {
+			int zu_behalten = haeufigste_ziffer(zahlenBackup, i);
+			for (int j = 0; j < zahlen.size(); j++) {
+				if(zu_behalten == 1) {
+					if(zahlen.get(j).substring(i).startsWith("1") && zahlenBackup.size() > 1) {
+						zahlenBackup.remove(zahlen.get(j));
 					}
-				}
-			} else {
-				for (int j = 0; j < zahlen1.size(); j++) {
-					if(zahlen1.get(j).substring(i).startsWith("1")) {
-						zahlen1.remove(j);
-						j--;
+				} else {
+					if(zahlen.get(j).substring(i).startsWith("0") && zahlenBackup.size() > 1) {
+						zahlenBackup.remove(zahlen.get(j));
 					}
 				}
 			}
 		}
-		
-		c02 = Integer.parseInt(zahlen1.get(0), 2);
-		
+		c02 = Integer.parseInt(zahlenBackup.get(0), 2);
 		
 		System.out.println("Oxygen:\t" + oxygen);
 		System.out.println(Integer.toBinaryString(oxygen));
 		System.out.println("C02:\t" + c02);
-		String co2String = Integer.toBinaryString(~oxygen);
-		System.out.println(co2String.substring(co2String.length()-12, co2String.length()));
+		String co2String = Integer.toBinaryString(c02);
+		System.out.println(co2String);
 	
-//		System.out.println("Ergebnis: " + (gamma * epsilon));
+		System.out.println("Ergebnis: " + (oxygen * c02));
 	}
 
 }
